f4bab264aea761f95d1e13a7bae3e25fdc1a8a57
Cleiton Dev <cleiton.sillva81@gmail.com>
2023-10-23 17:50:49 -0300

    fix: validacoes add + rm do address

--- src/controller/usuario.controller.js
+++ src/controller/usuario.controller.js
@@ -85,10 +85,12 @@ const addUserAddressController = async (req, res) => {
         req.body.createdAt = new Date();
         const endereco = await userService.addUserAddressService(req.params.id, req.body);
 
-        if(endereco.ok == 1){
-            res.status(201).send({ message: ` Endereço adicionado com Sucesso!`});  
-        }else{
+        console.log(endereco);
+
+        if(endereco.value == null){
             res.status(400).send({ message: `Algo deu errado no endereço, tente novemente!`});   
+        }else{
+            res.status(201).send({ message: ` Endereço adicionado com Sucesso!`});  
         }
 
     }catch (err){
@@ -100,6 +102,13 @@ const addUserAddressController = async (req, res) => {
 const removeUserAddressController = async (req, res) => {
     try{
         const endereco  = await userService.removeUserAddressService(req.body.id, req.body.addressId);
+        let found = false;
+
+        endereco.value.enderecos.map((valor, chave) => {
+            if(valor._id == req.body.addressId){
+                found = true;
+            }
+        })
 
         if(endereco.ok == 1){
             res.status(200).send({ message: ` Endereço removido com Sucesso!`});  
