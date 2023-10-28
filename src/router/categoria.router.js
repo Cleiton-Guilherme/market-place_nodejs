1c2d7d42e9eaeca84432aa53f85bd4339a06851a
Cleiton Dev <cleiton.sillva81@gmail.com>
2023-10-28 16:41:08 -0300

    fix: nome da rota de delete ajustado e module exports

--- src/router/categoria.router.js
+++ src/router/categoria.router.js
@@ -12,4 +12,6 @@ router.put("/update/:id", authMiddleware, categoriaController.updateCategoriaCon
 
 router.delete("/delete/:id", authMiddleware, categoriaController.deleteCategoriaController);
 
+module.exports = router;
+
 
